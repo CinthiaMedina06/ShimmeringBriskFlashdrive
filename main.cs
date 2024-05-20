f962ccad57f0e89848238c6cb1c4ba2032f38b94
CinthiaMedina06 <medinavac1@gmail.com>
2024-05-20 16:36:11 +0000

    Test #2 Expected outcome

--- main.cs
+++ main.cs
@@ -3,11 +3,11 @@ using System;
 class Program {
   public static void Main (string[] args) {
     Console.WriteLine ("PersonalInfo");
-    Concatenate("Lucinda", "Potter");
-    string birthDate = "6/24/1992";
+    Concatenate("John", "Smith");
+    string birthDate = "8/15/1978";
     Console.WriteLine (birthDate);
-    Concatenate("work","000-000-0101");
-    Concatenate("cell","000-000-0189");
+    Concatenate("work","000-111-0101");
+    Concatenate("cell","000-111-0189");
     }
 
   private static void Concatenate(string string1, string string2) {
