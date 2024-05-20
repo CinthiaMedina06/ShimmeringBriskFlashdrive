d4c60a755803a771b000d49d00ddfc7ccd336618
CinthiaMedina06 <medinavac1@gmail.com>
2024-05-20 16:10:50 +0000

    Made code changes lines 5-14

--- main.cs
+++ main.cs
@@ -2,6 +2,15 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World!");
+    Concatenate ("Program Name:","PersonalInfo");
+    Concatenate("Sally", "Smith");
+    string birthDate = "06/24/1992";
+    Console.WriteLine (birthDate);
+    Concatenate("work","555-555-5555");
+    Concatenate("cell","235-555-5855");
+    }
+
+  private static void Concatenate(string string1, string string2) {
+    Console.WriteLine (string1 + " " + string2);
   }
 }
