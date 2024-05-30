9349d49af96df438a65f042606c5eb3b1cc4ad03
CinthiaMedina06 <medinavac1@gmail.com>
2024-05-30 22:28:59 +0000

    Changed the code inside InchToCentmeters.cs

--- InchesToCentmeters.cs
+++ InchesToCentmeters.cs
@@ -3,19 +3,8 @@ using static System.Console;
 public class InchesToCentmeters{
   public void InchtoCm () {
     const double INCH = 2.54;
-    WriteLine("Enter number of inches to convert to centimeters");
-    var x = ReadLine();
-
-    var doubleValue = 0.00;
-    if (double.TryParse(x, out doubleValue)) {
-      var cm = doubleValue * INCH;
+    double x = 15994;
+    var cm = INCH * x;
       WriteLine("{0}{1}{2}{3}", x, " inches is ", cm, " centimeters");
-    }
-    else
-    {
-      WriteLine("Only numbers are allowed");
-      InchtoCm();
-    }
-
     }
 }
